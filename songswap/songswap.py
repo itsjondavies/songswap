6893effa7827194c274c664a5aa377c5d35c1fe8
itsjondavies <59796772+itsjondavies@users.noreply.github.com>
2023-07-14 21:32:16 +0100

    rewrite songswap module to enforce WAV files and update pyproject.toml

--- songswap/songswap.py
+++ songswap/songswap.py
@@ -1,11 +1,15 @@
+from __future__ import annotations
+
 import logging
 import os
 import sys
+from enum import Enum
 
-import ffmpy
 import librosa
 import moviepy.editor as mpe
+import pydub
 import pytube
+from moviepy.video.fx.multiply_speed import multiply_speed
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -16,15 +20,75 @@ formatter = logging.Formatter("%(message)s")
 stdout_handler.setFormatter(formatter)
 logger.addHandler(stdout_handler)
 
+_TEMPO_TOL = 0.15
+
+class FileExtension(Enum):
+    WAV = 1
+    MP3 = 2
+    MP4 = 3
+
+    @staticmethod
+    def from_filepath(filepath: str):
+        file_split = filepath.split(".")
+        file_ext = f".{file_split[-1]}"
+        if ".mp4" == file_ext:
+            return FileExtension.MP4
+        elif ".mp3" == file_ext:
+            return FileExtension.MP3
+        elif ".wav" == file_ext:
+            return FileExtension.WAV
+        else:
+            raise ValueError(f"Cannot infer file extension from: {file_ext}")
 
 def download_video(
         url: str,
-        output_path: str,
+        output_dir: str,
         audio_only: bool=False
 ) -> pytube.YouTube:
-    video = pytube.YouTube(url=url)
+    """
+    Download a video from YouTube using the pytube package.
+
+    Parameters
+    ----------
+    url : string
+        The url of the YouTube video to download.
+    output_dir : string
+        The local file directory (not full path) where the video should be downloaded to.
+    audio_only : bool
+        Whether to download the video and audio, or audio only.
+
+    Returns
+    -------
+    video : pytube.YouTube
+        The pytube YouTube object for the YouTube video.
+    download_path : str
+        The local file path where the video has been downloaded to.
+
+    Raises
+    ------
+    ValueError
+        If cannot load pytube.YouTube object from given URL
+
+    References
+    ----------
+    .. [1] pytube docs - https://pytube.io/en/latest/
+
+    Examples
+    --------
+    >>> vid, filepath = download_video(
+    >>>    url="https://www.youtube.com/watch?v=xmr8bEuyQjA",
+    >>>    output_path=out_path,
+    >>>    audio_only=True
+    >>> )
+
+    """
+    try:
+        video = pytube.YouTube(url=url)
+    except Exception as e:
+        raise ValueError("Could not load pytube.YouTube object from url") from e
+
     if audio_only:
-        streams = video.streams.filter(only_audio=True)
+        best_stream = video.streams.get_audio_only()
     else:
         streams = (
             video.streams
@@ -32,16 +96,90 @@ def download_video(
             .order_by("resolution")
             .desc()
         )
-    best_stream = streams.first()
-    path_components = output_path.split("/")
-    filename = path_components[-1]
-    path = "/".join(path_components[:-1])
-    best_stream.download(
-        output_path=path,
-        filename=filename
-    )
-    logger.info(f"Downloaded {video.title} to {output_path}/{filename}.")
-    return video
+        best_stream = streams.first()
+
+    download_path = best_stream.download(
+        output_path=output_dir
+    ).replace("\\","/")
+    logger.info(f"Downloaded {video.title} to {download_path}.")
+    return video, download_path
+
+class WAVFile:
+    def __init__(self, filepath: str) -> None:
+        # get filepath info attributes
+        self.filepath = filepath
+        self.file_extension = FileExtension.from_filepath(filepath)
+        self.filepath_root = ".".join(filepath.split(".")[:-1])
+        self.filename = self.filepath_root.split("/")[-1]
+        self._set_librosa_attributes()
+        self.mpe_clip = mpe.AudioFileClip(self.filepath)
+
+    @classmethod
+    def from_any_file(cls, filepath: str, wav_filepath: str=None):
+        file_extension = FileExtension.from_filepath(filepath)
+        if file_extension is FileExtension.MP4:
+            audio = pydub.AudioSegment.from_file(filepath, "mp4")
+        elif file_extension is FileExtension.MP3:
+            audio = pydub.AudioSegment.from_mp3(filepath)
+        elif file_extension is FileExtension.WAV:
+            if wav_filepath:
+                audio = pydub.AudioSegment.from_wav(filepath)
+            else:
+                return cls(filepath)
+
+        if not wav_filepath:
+            wav_filepath = f"{'.'.join(filepath.split('.')[:-1])}.wav"
+        audio.export(wav_filepath, format="wav")
+        return cls(wav_filepath)
+
+    def _set_librosa_attributes(self) -> None:
+        audio_array, sampling_rate = librosa.load(self.filepath)
+        self.tempo, self.beats = librosa.beat.beat_track(
+            y=audio_array,
+            sr=sampling_rate
+        )
+        self.duration = librosa.get_duration(y=audio_array, sr=sampling_rate)
+
+    def compare_tempo(self, wav_file: WAVFile):
+        tempo_diff = abs(self.tempo - wav_file.tempo) / self.tempo
+        return tempo_diff
+
+    def get_mean_tempo(self, wav_file: WAVFile):
+        goal_tempo = (self.tempo + wav_file.tempo) / 2
+        self_tempo_multi = goal_tempo / self.tempo
+        comp_tempo_multi = goal_tempo / wav_file.tempo
+        return goal_tempo, self_tempo_multi, comp_tempo_multi
+
+    def change_tempo(self, tempo_multiplier: float) -> WAVFile:
+        out_path = f"{self.filepath_root}_newtempo.wav"
+        new_clip = self.mpe_clip.time_transform(
+            lambda t: tempo_multiplier * t,
+            apply_to=["audio"]
+        )
+        # make new clip with different name to avoid garbage collection closing connection
+        # not sure why this happens with time transform function only
+        # also not sure why this only happens with audio and not video
+        new_clip2 = new_clip.with_duration(self.mpe_clip.duration / tempo_multiplier)
+        new_clip2.write_audiofile(out_path)
+        return WAVFile(out_path)
+
+    def create_subclip(self, start_time, end_time) -> WAVFile:
+        out_path = f"{self.filepath_root}_subclip.wav"
+        new_mpe_clip = self.mpe_clip.subclip(start_time=start_time, end_time=end_time)
+        new_mpe_clip.write_audiofile(out_path)
+        return WAVFile(out_path)
+
+    def close(self):
+        self.mpe_clip.close()
+
+    def delete(self):
+        self.close()
+        os.remove(self.filepath)
+
+def clean_up_files(filepaths: list) -> None:
+    for filepath in filepaths:
+        os.remove(filepath)
+
 
 def swap_songs(
         project_name: str,
@@ -50,131 +188,71 @@ def swap_songs(
 ) -> None:
     # set project filepaths
     project_dir = f"songswap_outputs/{project_name}"
-    base_video_path = f"{project_dir}/base_video.mp4"
-    new_audio_path = f"{project_dir}/new_audio.mp3"
-    alt_base_video_path = f"{project_dir}/alt_base_video.mp4"
-    alt_new_audio_path = f"{project_dir}/alt_new_audio.mp3"
-    final_audio_path = f"{project_dir}/final_audio.wav"
+    # alt_base_video_path = f"{project_dir}/alt_base_video.mp4"
     final_video_path = f"{project_dir}/{project_name}.mp4"
     # download base video
-    base_yt = download_video(video_url, base_video_path)
+    _, base_video_path = download_video(video_url, project_dir)
     # download audio from new video
-    new_yt = download_video(audio_url, new_audio_path, audio_only=True)
-    # check the tempos are compatible
-    old_audio_clip = AudioClip(base_video_path, base_yt.title)
-    new_audio_clip = AudioClip(new_audio_path, new_yt.title)
-    check_tempo_match(old_audio_clip, new_audio_clip)
+    _, new_audio_path = download_video(
+        audio_url,
+        project_dir,
+        audio_only=True
+    )
+    # convert audio to WAV - make future work easier
+    logger.info("Converting files to WAV format.")
+    base_wav = WAVFile.from_any_file(base_video_path)
+    new_wav = WAVFile.from_any_file(new_audio_path)
+    logger.info("Checking tempo...")
+    tempo_diff = base_wav.compare_tempo(new_wav)
+    if tempo_diff > _TEMPO_TOL:
+        raise ValueError(
+            f"Tempo difference of {tempo_diff:.2f} is greater than"
+            f"allowed value of {_TEMPO_TOL}."
+        )
+    else:
+        logger.info("SUCCESS! Tempo's are compatible.")
+
     # change tempos of clips
-    goal_tempo = (old_audio_clip.tempo + new_audio_clip.tempo) / 2
-    new_vid_tempo_multi = goal_tempo / old_audio_clip.tempo
-    new_audio_tempo_multi = goal_tempo / new_audio_clip.tempo
-    alter_media_speed(
-        input_path=base_video_path,
-        output_path=alt_base_video_path,
-        tempo_multi=new_vid_tempo_multi,
-        is_audio=False
+    _, base_tempo_multi, new_tempo_multi = base_wav.get_mean_tempo(
+        wav_file=new_wav
     )
-    alter_media_speed(
-        input_path=new_audio_path,
-        output_path=alt_new_audio_path,
-        tempo_multi=new_audio_tempo_multi,
-        is_audio=True
-    )
+    base_mpe = mpe.VideoFileClip(base_video_path, audio=False)
+    base_new_tempo = multiply_speed(base_mpe, base_tempo_multi)
+
+    base_wav_new_tempo = base_wav.change_tempo(base_tempo_multi)
+    new_wav_new_tempo = new_wav.change_tempo(new_tempo_multi)
     # verify changed tempos
-    alt_old_audio = AudioClip(alt_base_video_path, base_yt.title)
-    alt_new_audio = AudioClip(alt_new_audio_path, new_yt.title)
     logger.info(
-        f"Altered '{alt_old_audio.name}' tempo: {alt_old_audio.tempo:.2f}"
+        f"Altered '{base_wav_new_tempo.filename}' tempo: {base_wav_new_tempo.tempo:.2f}"
     )
     logger.info(
-        f"Altered '{alt_new_audio.name}' tempo: {alt_new_audio.tempo:.2f}"
+        f"Altered '{new_wav_new_tempo.filename}' tempo: {new_wav_new_tempo.tempo:.2f}"
     )
 
     # work out final audio/video clippings
-    alt_old_audio_mpe = alt_old_audio.get_mpe_object()
-    alt_new_audio_mpe = alt_new_audio.get_mpe_object()
-    new_start = alt_new_audio.beats[1] - alt_old_audio.beats[0]
-    new_audio_duration = alt_new_audio_mpe.duration - new_start
-
-    final_duration = min(alt_old_audio_mpe.duration, new_audio_duration)
-    final_audio = alt_new_audio_mpe.subclip(
+    new_start = new_wav_new_tempo.beats[1] - base_wav_new_tempo.beats[0]
+    new_audio_duration = new_wav_new_tempo.duration - new_start
+    final_duration = min(base_wav_new_tempo.duration, new_audio_duration)
+    final_wav = new_wav_new_tempo.create_subclip(
         start_time=new_start,
         end_time=new_start+final_duration
     )
-    final_audio.write_audiofile(final_audio_path)
 
     # stitch audio and video together
-    final_video_mpe = mpe.VideoFileClip(alt_base_video_path, audio=False)
-    final_video_mpe = final_video_mpe.with_end(final_audio.duration)
-    final_video_mpe = final_video_mpe.with_audio(final_audio)
+    final_video_mpe = base_new_tempo.with_end(final_wav.duration)
+    final_video_mpe = final_video_mpe.with_audio(final_wav.mpe_clip)
     final_video_mpe.write_videofile(final_video_path, audio_codec="aac")
 
     # close all readers and cleanup
-    alt_new_audio_mpe.close()
-    alt_old_audio_mpe.close()
-    final_audio.close()
+    base_wav.delete()
+    new_wav.delete()
+    base_wav_new_tempo.delete()
+    new_wav_new_tempo.delete()
+    final_wav.delete()
+    base_mpe.close()
+    base_new_tempo.close()
     final_video_mpe.close()
     clean_up_files([
         base_video_path,
         new_audio_path,
-        alt_base_video_path,
-        alt_new_audio_path,
-        final_audio_path,
     ])
-
-
-class AudioClip:
-    def __init__(self, file: str, name: str) -> None:
-        self.filepath = file
-        self.name = name
-        self.audio_array, self.sampling_rate = librosa.load(file)
-        self.tempo, self.beats = librosa.beat.beat_track(
-            y=self.audio_array,
-            sr=self.sampling_rate
-        )
-
-    def get_mpe_object(self) -> mpe.AudioFileClip:
-        return mpe.AudioFileClip(self.filepath)
-
-
-def check_tempo_match(
-        audio1: AudioClip,
-        audio2: AudioClip,
-        tempo_tol: float=0.15
-) -> None:
-    logger.info(f"{audio1.name} tempo: {audio1.tempo:.2f}")
-    logger.info(f"{audio2.name} tempo: {audio2.tempo:.2f}")
-    tempo_diff = abs(audio1.tempo - audio2.tempo) / audio1.tempo
-    if tempo_diff > tempo_tol:
-        raise ValueError(
-            f"Tempo difference of {tempo_diff:.2f} is greater than"
-            f"allowed value of {tempo_tol}. Try increasing tempo_tol "
-            f"parameter."
-        )
-    else:
-        logger.info("Tempo's compatible.")
-
-
-def alter_media_speed(
-        input_path: str,
-        output_path: str,
-        tempo_multi: str,
-        is_audio: bool=False
-) -> None:
-    if is_audio:
-        output_arg = ["-filter", f"atempo={tempo_multi}"]
-    else:
-        output_arg = (
-            f'-filter_complex "[0:v]setpts={1/tempo_multi}*PTS[v];[0:a]'
-            f'atempo={tempo_multi}[a]" -map "[v]" -map "[a]"'
-        )
-    ff = ffmpy.FFmpeg(
-        inputs={input_path: None},
-        outputs={output_path: output_arg}
-    )
-    ff.run()
-
-
-def clean_up_files(filepaths: list) -> None:
-    for filepath in filepaths:
-        os.remove(filepath)
