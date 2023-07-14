33937c6b7a5b5eb1929a9c2d0c05cf5985488c08
itsjondavies <59796772+itsjondavies@users.noreply.github.com>
2023-07-14 21:32:25 +0100

    update testing suite

--- tests/test_download.py
+++ tests/test_download.py
@@ -4,22 +4,22 @@ from songswap.songswap import download_video
 
 
 def test_download_video():
-    out_path = "tests/data/test_video.mp4"
-    download_video(
+    output_dir = "tests/outputs"
+    _, out_file = download_video(
         url="https://www.youtube.com/watch?v=WpmILPAcRQo",
-        output_path="tests/data/test_video.mp4",
+        output_dir=output_dir,
         audio_only=False
     )
-    assert os.path.isfile(out_path)
-    os.remove(out_path)
+    assert os.path.isfile(out_file)
+    os.remove(out_file)
 
 
 def test_download_audio():
-    out_path = "tests/data/test_audio.mp3"
-    download_video(
-        url="https://www.youtube.com/watch?v=WpmILPAcRQo",
-        output_path="tests/data/test_audio.mp3",
+    output_dir = "tests/outputs"
+    _, out_file = download_video(
+        url="https://www.youtube.com/watch?v=xmr8bEuyQjA",
+        output_dir=output_dir,
         audio_only=True
     )
-    assert os.path.isfile(out_path)
-    os.remove(out_path)
+    assert os.path.isfile(out_file)
+    os.remove(out_file)
